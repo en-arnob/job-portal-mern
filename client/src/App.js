88f2730bd51b4d6010a65796c39c0b29628b6b07
en-arnob <en.arnob@gmail.com>
2022-05-09 12:32:39 +0600

    tailwind and router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className='text-4xl text-red-800 p-4'>Home</div>;
+};
+
+export default Home;
