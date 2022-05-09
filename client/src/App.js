cc9dedc8adec9a7b9b8038b2709c1e298bb18213
Shahed Mahamud Lemon <lemonkhaan1@gmail.com>
2022-05-09 17:23:10 +0600

    2 different login page added
    2 different login page added for both client and candidate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import LoginCandidates from "./pages/LoginCandidates";
+import LoginClients from "./pages/LoginClients";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' exact element={<Home />} />
+        {/* <Route path="/" exact element={<Home />} /> */}
+        {/* <Route path="/" exact element={<LoginCandidates />} /> */}
+        <Route path="/" exact element={<LoginClients />} />
       </Routes>
     </Router>
   );
