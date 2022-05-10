cf2e3c1734a1304a76797c1373184060c796fb33
Shahed Mahamud Lemon <lemonkhaan1@gmail.com>
2022-05-10 16:39:31 +0600

    2 registration page added
    2 registration page added

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Nav from "./pages/components/Nav";
 import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/login' exact element={<LoginPage />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/reg" exact element={<RegistrationPage />} />
       </Routes>
     </Router>
   );
