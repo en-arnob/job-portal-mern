52d35ec71e25d3ac2b28019cd3f599683b6c5ff6
Shahed Mahamud Lemon <lemonkhaan1@gmail.com>
2022-05-20 12:39:03 +0600

    Dynamic EditProfile page and Some changes on edit candidates page(Not completed)
    Dynamic EditProfile page and Some changes on edit candidates page(Not completed)

--- client/src/pages/components/EditCandidatesProfile.jsx
+++ client/src/pages/components/EditCandidatesProfile.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { UsersContext } from "../../hooks/UsersContext";
+import { useNavigate } from "react-router-dom";
 import { Col, Container, Row, Card } from "react-bootstrap";
 import EditImg from "../../assets/images/EditCandidates.svg";
 
 const EditCandidatesProfile = () => {
+  const [data, setData] = useState({});
+  const [user, setUser] = useContext(UsersContext);
+  const [error, setError] = useState(" ");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = `http://127.0.0.1:8000/userDetails/${user.id}/${user.usertype}`;
+      const { data: res } = await axios.patch(url, data);
+      navigate("/");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.errors[0]);
+      }
+    }
+  };
   return (
     <div>
       <Container>
@@ -15,7 +42,7 @@ const EditCandidatesProfile = () => {
           </Col>
           <Col lg={3} md={12} sm={12}></Col>
         </Row>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Row>
             <div className="text-center">
               <p className="font-mono text-3xl font-extrabold text-blue-700">
@@ -26,10 +53,7 @@ const EditCandidatesProfile = () => {
             <Col lg={6} md={6} sm={12}>
               <div className="mt-5">
                 <Card.Body className="text-center">
-                  <div
-                    class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                    //   onSubmit={handleSubmit}
-                  >
+                  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div class="mb-4">
                       <label
                         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -183,42 +207,37 @@ const EditCandidatesProfile = () => {
                           </div>
                         </div>
                       </div>
-                    </div>
 
-                    <div class="mb-6">
                       <label
-                        class="block text-gray-700 text-sm font-bold mb-2"
-                        for="password"
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-cover"
                       >
-                        Password
+                        Cover letter
                       </label>
-                      <input
+                      <textarea
                         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-password"
-                        type="password"
-                        placeholder="******************"
-                        //   onChange={handleChange}
-                        //   value={data.password}
-                        name="password"
-                      />
-                      <label
-                        class="block text-gray-700 text-sm font-bold mb-2"
-                        for="password"
-                      >
-                        Confirm Password
-                      </label>
-                      <input
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-password"
-                        type="password"
-                        placeholder="******************"
+                        id="grid-cover"
+                        type="text"
                         // onChange={handleChange}
-                        // value={data.confirmPassword}
-                        // name="confirmPassword"
+                        // value={data.fullname}
+                        name="cover"
+                        placeholder="Write a cover letter"
                       />
-                      {/* {error && (
-                      <p class="text-red-500 text-xs italic">{error.msg}</p>
-                    )} */}
+                      <label
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-bio"
+                      >
+                        Bio
+                      </label>
+                      <textarea
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                        id="grid-experience"
+                        type="text"
+                        // onChange={handleChange}
+                        // value={data.fullname}
+                        name="bio"
+                        placeholder="Write your bio"
+                      />
                     </div>
                   </div>
                 </Card.Body>
@@ -307,36 +326,6 @@ const EditCandidatesProfile = () => {
                         // value={data.fullname}
                         name="experience"
                         placeholder="Enter experinces"
-                      />
-                      <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-cover"
-                      >
-                        Cover letter
-                      </label>
-                      <textarea
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-cover"
-                        type="text"
-                        // onChange={handleChange}
-                        // value={data.fullname}
-                        name="cover"
-                        placeholder="Write a cover letter"
-                      />
-                      <label
-                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-bio"
-                      >
-                        Bio
-                      </label>
-                      <textarea
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-experience"
-                        type="text"
-                        // onChange={handleChange}
-                        // value={data.fullname}
-                        name="bio"
-                        placeholder="Write your bio"
                       />
 
                       <label
