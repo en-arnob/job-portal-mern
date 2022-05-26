d0d9b44dface661db5587d131b16a4332f27e6a0
en-arnob <en.arnob@gmail.com>
2022-05-26 11:58:51 +0600

    Update AppliedJobs.jsx

--- client/src/pages/AppliedJobs.jsx
+++ client/src/pages/AppliedJobs.jsx
@@ -1,54 +1,87 @@
 import React from "react";
+import jwt_decode from "jwt-decode";
+import { MdMessage } from "react-icons/md";
+import { Col, Container, Row, Card } from "react-bootstrap";
+import { HiOutlineOfficeBuilding } from "react-icons/hi";
 
 const AppliedJobs = () => {
   const token = localStorage.getItem("myToken");
+  let usr = "";
+  if (token) {
+    usr = jwt_decode(token).user;
+  }
+
   return (
-    <header class='bg-gray-50'>
-      <div class='max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8'>
-        <div class='sm:justify-between sm:items-center sm:flex'>
-          <div class='text-center sm:text-left'>
-            <h1 class='text-2xl font-bold text-gray-900 sm:text-3xl'>
-              Welcome Back, Barry!
-            </h1>
+    <div>
+      <header className='bg-gray-50 mb-6'>
+        <div class='max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8'>
+          <div class='sm:justify-between sm:items-center sm:flex'>
+            <div class='text-center sm:text-left'>
+              <h1 class='text-2xl font-bold text-gray-900 sm:text-3xl'>
+                {usr.fullname}
+              </h1>
 
-            <p class='mt-1.5 text-sm text-gray-500'>
-              Let's write a new blog post! 🎉
-            </p>
-          </div>
+              <p class='mt-1.5 text-sm text-gray-500'>
+                Your applied jobs are listed bellow
+              </p>
+            </div>
 
-          <div class='flex flex-col gap-4 mt-4 sm:flex-row sm:mt-0 sm:items-center'>
-            <button
-              class='inline-flex items-center justify-center px-5 py-3 text-gray-500 transition bg-white border border-gray-200 rounded-lg hover:text-gray-700 focus:outline-none focus:ring'
-              type='button'
-            >
-              <span class='text-sm font-medium'> View Website </span>
-
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                class='w-4 h-4 ml-1.5'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-                stroke-width='2'
+            <div class='flex flex-col gap-4 mt-4 sm:flex-row sm:mt-0 sm:items-center'>
+              <button
+                class='flex gap-2  items-center justify-center px-5 py-3 text-gray-500 transition bg-white border border-gray-200 rounded-lg hover:text-gray-700 focus:outline-none focus:ring'
+                type='button'
               >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
-                />
-              </svg>
-            </button>
+                <span class='text-sm font-medium'> Message Box </span>
 
-            <button
-              class='block px-5 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring'
-              type='button'
-            >
-              Create Post
-            </button>
+                <MdMessage className='text-xl mt-1' />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <Container>
+        <Row>
+          <Col lg={12} md={6} sm={12}>
+            <div className=' rounded-lg bg-gray-100'>
+              <Card.Body>
+                <Card.Title>Cyber Security Specialist</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  <p className='flex gap-3 font-normal text-sm  text-blue-700'>
+                    <HiOutlineOfficeBuilding className='text-sm mt-1' /> Google
+                  </p>
+                </Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+
+                <dl class='flex mt-6'>
+                  <div class='flex flex-col-reverse'>
+                    <dd class='text-sm  text-red-800'>Deadline: 20-11-2022</dd>
+                  </div>
+
+                  <div class='flex flex-col-reverse ml-3 sm:ml-6'>
+                    <dd class='text-sm  text-gray-800'>
+                      Total Applicants: 100
+                    </dd>
+                  </div>
+                </dl>
+                <div className='card-footer'>
+                  <div className='text-center'>
+                    <button class='bg-green-600 hover:bg-green-700 text-white font-bold mx-1 py-2 px-3 rounded-full'>
+                      View Job
+                    </button>
+                    <button class='bg-red-600 hover:bg-red-700 text-white font-bold mx-1 py-2 px-4 rounded-full'>
+                      Delete Application
+                    </button>
+                  </div>
+                </div>
+              </Card.Body>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
